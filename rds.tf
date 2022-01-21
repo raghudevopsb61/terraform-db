3d9ed6c49efa6263a6bb3a730585b02707abed64
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-21 07:20:55 +0530

    Update terraform-db/

--- rds.tf
+++ rds.tf
@@ -47,3 +47,16 @@ resource "aws_security_group" "mysql" {
   }
 }
 
+resource "null_resource" "mysql-schema" {
+  provisioner "local-exec" {
+    command = <<-EOF
+sudo yum install mariadb -y
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -h ${aws_db_instance.default.address} -u${jsondecode(data.aws_secretsmanager_secret_version.dev-secrets.secret_string)["RDS_MYSQL_USER"]} -p${jsondecode(data.aws_secretsmanager_secret_version.dev-secrets.secret_string)["RDS_MYSQL_PASS"]} <shipping.sql
+EOF
+  }
+}
+
