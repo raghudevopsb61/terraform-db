cfa4c8c3fcca7c2d16cef826da71e668b03c712e
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-21 07:04:03 +0530

    Update terraform-db/

--- elasticache.tf
+++ elasticache.tf
@@ -1,13 +1,12 @@
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "redis-${var.ENV}"
-  engine               = "redis"
-  node_type            = "cache.t3.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.2"
-  engine_version       = "6.2"
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
-  security_group_ids   = [aws_security_group.redis.id]
+  cluster_id         = "redis-${var.ENV}"
+  engine             = "redis"
+  node_type          = "cache.t3.micro"
+  num_cache_nodes    = 1
+  engine_version     = "6.x"
+  port               = 6379
+  subnet_group_name  = aws_elasticache_subnet_group.subnet-group.name
+  security_group_ids = [aws_security_group.redis.id]
 }
 
 resource "aws_elasticache_subnet_group" "subnet-group" {
