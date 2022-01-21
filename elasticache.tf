0f692d204c21c17728500a4dcc47b05d5384cac4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-21 07:25:32 +0530

    Update terraform-db/

--- elasticache.tf
+++ elasticache.tf
@@ -43,3 +43,11 @@ resource "aws_security_group" "redis" {
 output "test" {
   value = aws_elasticache_cluster.redis
 }
+
+resource "aws_route53_record" "redis" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "redis-${var.ENV}.roboshop.internal"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
+}
